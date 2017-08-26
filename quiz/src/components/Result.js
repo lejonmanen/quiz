af429b456a520c439193a08d29dee4c0698b633d
David Andersson <lejonmanen@gmail.com>
2017-08-26 15:06:01 +0200

    Version 0.1, ask questions and register votes

--- quiz/src/components/Result.js
+++ quiz/src/components/Result.js
@@ -9,14 +9,15 @@ function Result(props) {
 	//const answers = props.party.map( o => o.answer )
 	//	.sort().filter( (a, i, list) => list.indexOf(a) === i );
 	//let numParticipants = answers.length;
+	const toPercent = (num, total) => Math.round(num / total * 100);
 	console.log('Result party:', props.party);
-	let numAnswers = props.party.reduce( (prev, cur) => cur.answer > 0 ? 1 : 0, 0 );
+	let numAnswers = props.party.reduce( (prev, cur) => prev + (cur.answer > 0 ? 1 : 0), 0 );
 	console.log('Result num:', numAnswers);
 	const options = props.q.options.map( (op, i) => 
-		(<li key={op+i} > {op}: {props.party.reduce( (prev, cur) => cur === i ? 1 : 0, 0 )} röster, {Math.round(props.party.reduce( (prev, cur) => cur === i ? 1 : 0, 0 ) / Math.max(numAnswers, 1)*100)}% </li>) );
+		(<li key={op+i} > {op}: {props.party.reduce( (prev, cur) => prev+(cur.answer === i+1 ? 1 : 0), 0 )} röster, {toPercent(props.party.reduce( (prev, cur) => prev+(cur.answer === i+1 ? 1 : 0), 0 ), Math.max(numAnswers, 1))}% </li>) );
 	return (
 		<div>
-			Resultat: <br/>
+			Resultat: {numAnswers} av {props.party.length} har röstat, {toPercent(numAnswers, props.party.length)}% <br/>
 			<ol>
 				{options}
 			</ol>
